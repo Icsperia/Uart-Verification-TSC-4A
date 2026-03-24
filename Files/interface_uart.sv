b8514219939715bd17af6e419a768d2c30a490ce
Husu Bianca <126001578+HusuBia@users.noreply.github.com>
2026-03-24 16:25:57 +0200

    asertii interfata uart

--- Files/interface_uart.sv
+++ Files/interface_uart.sv
@@ -3,47 +3,97 @@
 //-------------------------------------------------------------------------
 interface intf_uart(input logic clk,reset);
   
-  //declaring the signals
-  logic tx;
-  //logic wr_en;
-  //logic rd_en;
-  //logic [7:0] wdata;
-  //logic [7:0] rdata;
-  //semnalele din clocking block sunt ncrone cu frontul crescator de ceas
-  //driver clocking block
-
-  //nu avem driver la uart
-  //monitor clocking block
-  clocking monitor_cb @(posedge clk);
-    default input #1 output #1;
-    input tx; 
-  endclocking
-
-  //monitor modport  
-  modport MONITOR (clocking monitor_cb,input clk,reset);
+    //declaring the signals
+    logic tx;
+    //logic wr_en;
+    //logic rd_en;
+    //logic [7:0] wdata;
+    //logic [7:0] rdata;
+    //semnalele din clocking block sunt ncrone cu frontul crescator de ceas
+    //driver clocking block
     
-        //asertii pe interfata
+    //nu avem driver la uart
+    //monitor clocking block
+    clocking monitor_cb @(posedge clk);
+      default input #1 output #1;
+      input tx; 
+    endclocking
+    
+    //monitor modport  
+    modport MONITOR (clocking monitor_cb,input clk,reset);
+    
 	//nu avem voie sa avem si read si write in acelai timp
-   //property rd_wr;
-     //@(posedge clk) disable iff (reset==0)//daca avem reset, nu se executa asertia
-     //!(wr_en && rd_en);
-   //endproperty
-  
-  //asertia_rd_wr: assert property (rd_wr) 
-    //else $error("INTERFATA_INTRARE: a picat asertia asertia_rd_wr");
-    //RD_WR_C: cover property (rd_wr);//ne asiguram ca proprietatea a fost accesata macar o data
-      
-      
-      
-      	//daca wr_en a fost 1, in urmatorul tact de ceas va fi 0
-   //property wr_en_pulse;
-     //@(posedge clk) disable iff (reset==0)//daca avem reset, nu se executa asertia
-     //wr_en |=> !wr_en;
-  //endproperty
-  
-  //asertia_wr_en_pulse: assert property (wr_en_pulse) 
-    //else $error("INTERFATA_INTRARE: a picat asertia asertia_rd_wr");
-    //WR_EN_pulse: cover property (wr_en_pulse);//ne asiguram ca proprietatea a fost accesata macar o data
-      
-  
+   
+// WAIT_TRANSACTION 0
+// START            1
+// DATA             2
+// PARITY           3
+// STOP             4
+
+//verificare tx dupa reset
+    property tx_invalid;
+      @(posedge clk) disable iff (reset==0)//daca avem reset, nu se executa asertia
+      !$isunknown(tx);
+    endproperty
+    
+    asertia_tx_invalid: assert property (tx_invalid) 
+     else $error("UART_ERR: tx in Z sau X");
+    TX_invalid: cover property (tx_invalid);
+    
+//verificare tx in starea de repaus == 1
+	property tx_repaus;
+      @(posedge clk) disable iff (!rst_n)
+      (uart.current_state == `WAIT_TRANSACTION) |-> (tx == 1'b1);
+    endproperty
+	
+    asertia_tx_repaus: assert property (tx_repaus) 
+	  else $error("UART_ERR: tx nu este HIGH in WAIT_TRANSACTION");
+    
+//verificare bit de start == 0 
+    property tx_bit_start;
+      @(posedge clk) disable iff (reset==0)
+      (uart.current_state == `START) |-> (tx==1'b0);
+    endproperty
+    
+    asertia_tx_bit_start: assert property (tx_bit_start) 
+     else $error("UART_ERR: bit de start invalid");
+    TX_bit_start: cover property (tx_bit_start);
+	
+//verificare integritate date -> daca tx == bitul selectat din fifo
+	property tx_data_integrity;
+      @(posedge clk) disable iff (!rst_n)
+      (uart.current_state == `DATA) |-> (tx == uart.fifo[uart.r_counter][uart.bit_cnt]);
+    endproperty
+	
+    asertia_tx_data_integrity: assert property (tx_data_integrity) 
+	  else $error("UART_ERR: Datele de pe tx nu corespund cu bitul curent din FIFO");
+    
+//avem bit de paritate
+    property tx_parity_bit;
+        @(posedge clk) disable iff (!rst_n || uart.HAS_PARITY == 0)
+        (uart.current_state == `PARITY) |-> (tx == ^(uart.fifo[uart.r_counter]));
+    endproperty
+	
+    asertia_tx_parity_bit: assert property (tx_parity_bit) 
+	    else $error("UART_ERR: Bit de paritate calculat gresit");
+	
+//biti de stop
+    property tx_bit_stop;
+      @(posedge clk) disable iff (reset==0)
+      (uart.current_state == `STOP) |-> (tx==1'b1);
+    endproperty
+    
+    asertia_tx_bit_stop: assert property (tx_bit_stop) 
+     else $error("UART_ERR: bit de stop invalid");
+    TX_bit_stop: cover property (tx_bit_stop);
+	
+//stabilitatea bitului pe durata boud_rate_counter
+    property tx_bit_stability;
+        @(posedge clk) disable iff (!rst_n)
+        (uart.current_state != `WAIT_TRANSACTION && uart.boud_rate_counter > 0) |-> $stable(tx);
+    endproperty
+	
+    asertia_tx_bit_stability: assert property (tx_bit_stability) 
+	   else $error("UART_ERR: Semnalul tx s-a schimbat inainte de finalizarea baud rate");
+
 endinterface
