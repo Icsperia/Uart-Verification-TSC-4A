2d310b666aa61529deb04184164781ebb9de8302
Husu Bianca <126001578+HusuBia@users.noreply.github.com>
2026-04-01 11:08:51 +0300

    coverage

--- Files/coverage.sv
+++ Files/coverage.sv
@@ -1,55 +1,60 @@
-////prin coverage, putem vedea ce situatii (de exemplu, ce tipuri de tranzactii) au fost generate in simulare; astfel putem masura stadiul la care am ajuns cu verificarea
-//class coverage;
+//prin coverage, putem vedea ce situatii (de exemplu, ce tipuri de tranzactii) au fost generate in simulare; astfel putem masura stadiul la care am ajuns cu verificarea
+`include "transaction.sv"
+class coverage #(parameter DATA_WIDTH = 8);
   
-//  transaction trans_covered;
+  transaction trans_covered;
   
-//  //pentru a se putea vedea valoarea de coverage pentru fiecare element trebuie create mai multe grupuri de coverage, sau trebuie creata o functie de afisare proprie
-//  covergroup transaction_cg;
-//    //linia de mai jos este adaugata deoarece, daca sunt mai multe instante pentru care se calculeaza coverage-ul, noi vrem sa stim pentru fiecare dintre ele, separat, ce valoare avem.
-//    option.per_instance = 1;
-//    wr_enable_cp: coverpoint trans_covered.wr_en;
-//    rd_enable_cp: coverpoint trans_covered.rd_en;
+  localparam int MAX_VAL = (1 << DATA_WIDTH) - 1; 
+  localparam int MID_VAL = MAX_VAL / 2;
+  //pentru a se putea vedea valoarea de coverage pentru fiecare element trebuie create mai multe grupuri de coverage, sau trebuie creata o functie de afisare proprie
+  covergroup transaction_cg;
+    //linia de mai jos este adaugata deoarece, daca sunt mai multe instante pentru care se calculeaza coverage-ul, noi vrem sa stim pentru fiecare dintre ele, separat, ce valoare avem.
+    option.per_instance = 1;
+    valid_cp: coverpoint trans_covered.valid;
+    ready_cp: coverpoint trans_covered.ready;
     
-//    // adaugati adresele tuturor registrilor pe care ii aveti in DUT (sunt documentati in specificatie)
-//    // bin-ul other_addresses este important deoarece vrem sa vedem ca au fost trimise tranzactii si la adrese care nu apartin unor registrii (in acest caz DUT-ul trebuie sa aserteze semnalul pslverr)
-//     address_cp: coverpoint trans_covered.addr{
-//      bins addr_reg_config = {0};
-//      bins addr_reg_status = {1};
-//      bins other_addresses = default;
-//    }
+    // adaugati adresele tuturor registrilor pe care ii aveti in DUT (sunt documentati in specificatie)
+    // bin-ul other_addresses este important deoarece vrem sa vedem ca au fost trimise tranzactii si la adrese care nu apartin unor registrii (in acest caz DUT-ul trebuie sa aserteze semnalul pslverr)
+     tx_cp: coverpoint trans_covered.tx{
+      bins bins_start = {1};
+      bins bins_date = {2};
+      bins bins_paritate = {3};
+      bins bins_stop ={4};
+    }
     
-//    write_data_cp: coverpoint trans_covered.wdata {
-//      bins big_values = {[191:254]};
-//      bins medium_values = {[127:190]};
-//      bins low_values = {[1:126]};
-//      bins lowest_value = {0};
-//      bins highest_value = {255};
-//    }
-//    read_data_cp: coverpoint trans_covered.rdata {
-//      //prin linia de mai jos am impartit intervalul de valori in 4 intervale egale
-//      bins range[4] = {[0:$]};
-//      bins lowest_value = {0};
-//      bins highest_value = {255};
-//    }
-    
-//  endgroup
-//  //se creaza grupul de coverage; ATENTIE! Fara functia de mai jos, grupul de coverage nu va putea esantiona niciodata date deoarece pana acum el a fost doar declarat, nu si creat
-//  function new();
-//    transaction_cg = new();
-//  endfunction
+    data_i_cp: coverpoint trans_covered.data_i {
+      bins lowest_value  = {0};
+      bins highest_value = {MAX_VAL};
+      bins low_values    = {[1 : MID_VAL/2]};
+      bins medium_values = {[MID_VAL/2 + 1 : MID_VAL]};
+      bins big_values    = {[MID_VAL + 1 : MAX_VAL - 1]};
+    }
+
+    counter_cp: coverpoint trans_covered.counter {
+      bins pachet_complet[] = {[0 : DATA_WIDTH-1]}; 
+    }
+    handshake_cross: cross valid_cp, ready_cp;
+  endgroup
   
-//  task sample(transaction trans_covered); 
-//  	this.trans_covered = trans_covered; 
-//  	transaction_cg.sample(); 
-//  endtask:sample   
+  //se creaza grupul de coverage; ATENTIE! Fara functia de mai jos, grupul de coverage nu va putea esantiona niciodata date deoarece pana acum el a fost doar declarat, nu si creat
+  function new();
+    transaction_cg = new();
+  endfunction
   
-//  function print_coverage();
-//    $display ("Address coverage = %.2f%%", transaction_cg.address_cp.get_coverage());
-//    $display ("Write data coverage = %.2f%%", transaction_cg.write_data_cp.get_coverage());
-//    $display ("Read data coverage = %.2f%%", transaction_cg.read_data_cp.get_coverage());
-//    $display ("Overall coverage = %.2f%%", transaction_cg.get_coverage());
-//  endfunction
+  task sample_function(transaction trans_covered); 
+  	this.trans_covered = trans_covered; 
+  	transaction_cg.sample(); 
+  endtask: sample_function   
   
-//  //o alta modalitate de a incheia declaratia unei clase este sa se scrie "endclass: numele_clasei"; acest lucru este util mai ales cand se declara mai multe clase in acelasi fisier; totusi, se recomanda ca fiecare fisier sa nu contina mai mult de o declaratie a unei clase
-//endclass: coverage
+  function print_coverage();
+    $display ("Valid coverage = %.2f%%", transaction_cg.valid_cp.get_coverage());
+    $display ("Ready coverage = %.2f%%", transaction_cg.ready_cp.get_coverage());
+    $display ("TX line coverage = %.2f%%", transaction_cg.tx_cp.get_coverage());
+    $display ("Data in coverage = %.2f%%", transaction_cg.data_i_cp.get_coverage());
+    $display ("Counter coverage = %.2f%%", transaction_cg.counter_cp.get_coverage());
+    $display ("Overall coverage = %.2f%%", transaction_cg.get_coverage());
+  endfunction
+  
+  //o alta modalitate de a incheia declaratia unei clase este sa se scrie "endclass: numele_clasei"; acest lucru este util mai ales cand se declara mai multe clase in acelasi fisier; totusi, se recomanda ca fiecare fisier sa nu contina mai mult de o declaratie a unei clase
+endclass: coverage
 
--- Files/interface_valid_ready.sv
+++ Files/interface_valid_ready.sv
@@ -29,33 +29,77 @@ parameter DATA_WIDTH=8
   endclocking
   
   //driver modport
+  //driver modport
   modport DRIVER  (clocking driver_cb,input clk,reset);
   
   //monitor modport  
   modport MONITOR (clocking monitor_cb,input clk,reset);
-    /*
-        //asertii pe interfata
-	//nu avem voie sa avem si read si write in acelai timp
-   property rd_wr;
-     @(posedge clk) disable iff (reset==0)//daca avem reset, nu se executa asertia
-     !(wr_en && rd_en);
+  /* 1.daca valid_i(adica am date de trimis) a fost 1 si ready 0(inca nu pot primi),
+  valid_i trebuie sa ramana tot 1*/
+  property valid_stable;
+    @(posedge clk) disable iff (reset==0)
+    (valid && !ready) |=> valid;
+  endproperty
+ 
+ asertia_valid_stable: assert property (valid_stable) 
+    else $error("A picat asertia asertia_valid_stable");
+    VALID_STABLE_C: cover property (valid_stable);
+  
+  // 2.datele trebuie sa ramana stabile cat timp valid=1 si ready=0
+  property data_stabila;
+    @(posedge clk) disable iff (reset==0)
+    (valid && !ready) |=> $stable(data_i);
   endproperty
   
-  asertia_rd_wr: assert property (rd_wr) 
-    else $error("INTERFATA_INTRARE: a picat asertia asertia_rd_wr");
-    RD_WR_C: cover property (rd_wr);//ne asiguram ca proprietatea a fost accesata macar o data
-      
-      
-      
-      	//daca wr_en a fost 1, in urmatorul tact de ceas va fi 0
-   property wr_en_pulse;
-     @(posedge clk) disable iff (reset==0)//daca avem reset, nu se executa asertia
-     wr_en |=> !wr_en;
+  asertia_data_stabila: assert property (data_stabila) 
+    else $error("A picat asertia asertia_data_stabila");
+    DATA_STABILA_C: cover property (data_stabila);
+  
+  // 3.valid nu are voie sa fie x sau z, adica nedeterminate
+   property valid_cunoscut;
+    @(posedge clk) disable iff (reset==0)
+    !$isunknown(valid);
   endproperty
   
-  asertia_wr_en_pulse: assert property (wr_en_pulse) 
-    else $error("INTERFATA_INTRARE: a picat asertia asertia_rd_wr");
-    WR_EN_pulse: cover property (wr_en_pulse);//ne asiguram ca proprietatea a fost accesata macar o data
-     */ 
+  asertia_valid_cunoscut: assert property (valid_cunoscut) 
+    else $error("A picat asertia asertia_valid_cunoscut");
+    VALID_CUNOSCUT_C: cover property (valid_cunoscut);
+	
+	//4.ready nu are voie sa fie X sau Z (nedeterminat) niciodata
+  property ready_cunoscut;
+    @(posedge clk) disable iff (reset==0)
+    !$isunknown(ready);
+  endproperty
+
+  asertia_ready_cunoscut: assert property (ready_cunoscut)
+    else $error("A picat asertia asertia_ready_cunoscut");
+	
+	//5.data nu are voie sa fie X sau Z cat timp valid_i este 1
+  property data_cunoscuta;
+    @(posedge clk) disable iff (reset==0)
+    valid |-> !$isunknown(data_i);
+  endproperty
+
+  asertia_data_cunoscuta: assert property (data_cunoscuta)
+    else $error("A picat asertia asertia_data_cunoscuta");
+
+  //6.dupa iesirea din reset, valid_i trebuie sa fie 0
+  property valid_dupa_reset;
+    @(posedge clk)
+    $rose(reset) |-> !valid;
+  endproperty
   
+  asertia_valid_dupa_reset: assert property (valid_dupa_reset) 
+    else $error("A picat asertia asertia_valid_dupa_reset");
+    VALID_DUPA_RESET_C: cover property (valid_dupa_reset);
+  
+  //7.dupa iesirea din reset, ready_o trebuie sa fie 0
+  property ready_dupa_reset;
+    @(posedge clk)
+    $rose(reset) |-> !ready;
+  endproperty
+
+  asertia_ready_dupa_reset: assert property (ready_dupa_reset)
+    else $error("A picat asertia asertia_ready_dupa_reset");
+
 endinterface
